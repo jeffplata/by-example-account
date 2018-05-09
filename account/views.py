5ebbf24bc060b0df7250b97e948017cd600db3f0
Jeoffrey M. Plata <jmplata@nfa.gov.ph.local>
2018-05-09 18:14:09 +0800

    messages

--- account/views.py
+++ account/views.py
@@ -5,6 +5,7 @@ from .forms import LoginForm, UserRegistrationForm, \
     UserEditForm, ProfileEditForm
 from django.contrib.auth.decorators import login_required
 from .models import Profile
+from django.contrib import messages
 
 def user_login(request):
     if request.method == 'POST':
@@ -66,6 +67,10 @@ def edit(request):
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
+            messages.success(request, 'Profile updated '\
+                                      'successfully')
+        else:
+            messages.error(request, 'Error updating your profile')
     else:
         user_form = UserEditForm(instance=request.user)
         profile_form = ProfileEditForm(
@@ -74,3 +79,4 @@ def edit(request):
                   'account/edit.html',
                   {'user_form': user_form,
                    'profile_form': profile_form})
+                   
